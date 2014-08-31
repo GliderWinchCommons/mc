da01ad0c191ece79fb95c1acbf1b9cf7c4cc3d12
Donald <dhaselwood@verizon.net>
2014-08-30 21:43:35 -0400

    partially tested

---
+++ common_all/trunk/common_canid_et.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+* File Name          : common_canid_et.h
+* Date First Issued  : 08/30/2014
+* Board              : NA
+* Description        : CAN ID assignments for ET
+*******************************************************************************/
+
+
+#ifndef __COMMON_CANID_ET
+#define __COMMON_CANID_ET
+
+#define CANID_TIME	0x20000000	// 
+#define CANID_TORQUE	0x21400000; 	//
+
+
+
+#endif 
+
